2cd0d6a435174822d95960f8e3b0160211b07d67
No Name <nonameuserd007@outlook.com>
2025-02-15 12:16:56 -0500

    cleanups

--- types/global.d.ts
+++
@@ -1,7 +0,0 @@
-interface VideoFrame {
-  readonly format: VideoPixelFormat | null;
-  readonly timestamp: number;
-  readonly codedWidth: number;
-  readonly codedHeight: number;
-  close(): void;
-}
